ec1ec2acc4a85f48d4adcac5c01a90baaac69074
Lukasz Wojcik <lukasz.wojcik@softwareone.com>
2020-07-14 11:56:31 +0200

    create handler and controller

---
+++ backend/Alpaki.Logic/Handlers/UpdateUserData/UpdateUserDataRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Alpaki.Logic.Handlers.UpdateUserData
+{
+    public class UpdateUserDataRequestValidator : AbstractValidator<UpdateUserDataRequest>
+    {
+        public UpdateUserDataRequestValidator()
+        {
+            RuleFor(u => u.Email).EmailAddress().When(u => u.Email != null).WithMessage("Niepoprawny adres email.");
+            RuleFor(u => u.FirstName).MaximumLength(250).NotEmpty().When(u => u.FirstName != null).WithMessage("Imię nie może być dłuższe niż 250 znaków");
+            RuleFor(u => u.LastName).MaximumLength(250).NotEmpty().When(u => u.LastName != null).WithMessage("Nazwisko nie może być dłuższe niż 250 znaków");
+            RuleFor(u => u.Brand).NotEmpty().When(u => u.Brand != null).WithMessage("Oddział nie może być pusty");
+            RuleFor(u => u.PhoneNumber).NotEmpty().When(u => u.PhoneNumber != null).WithMessage("Numer telefonu nie może być pusty");
+        }
+    }
+}
---
+++ backend/Alpaki.WebApi/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Alpaki.Logic.Handlers.UpdateUserData;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Alpaki.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController: ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPatch]
+        [ProducesResponseType(typeof(UpdateUserDataResponse), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), (int)HttpStatusCode.BadRequest)]
+        public Task<UpdateUserDataResponse> UpdateUserData(UpdateUserDataRequest updateUserDataRequest) => _mediator.Send(updateUserDataRequest);
+    }
+}
