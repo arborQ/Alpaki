8690176168479d09593ad424000095c28ba03d1c
arborQ <arbor@o2.pl>
2020-07-17 14:31:41 +0200

    [#6] - add integrations test for volunteer query (#18)
    * [#6] - add integrations test for volunteer query
    
    * add filters
    
    Co-authored-by: Lukasz Wojcik <lukasz.wojcik@softwareone.com>

---
+++ backend/Alpaki.Tests.IntegrationTests/Fixtures/Builders/DreamCategoryFixtureBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Alpaki.Database.Models;
+using AutoFixture;
+using AutoFixture.Dsl;
+
+namespace Alpaki.Tests.IntegrationTests.Fixtures.Builders
+{
+    public static class DreamCategoryFixtureBuilder
+    {
+        public static IPostprocessComposer<DreamCategory> DreamCategoryBuilder(this Fixture fixture)
+        {
+            return fixture.Build<DreamCategory>()
+                .With(u => u.DreamCategoryId, 0)
+                .Without(u => u.Dreams);
+        }
+    }
+}
---
+++ backend/Alpaki.Tests.IntegrationTests/Fixtures/Builders/DreamFixtureBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Alpaki.CrossCutting.Enums;
+using Alpaki.Database.Models;
+using AutoFixture;
+using AutoFixture.Dsl;
+
+namespace Alpaki.Tests.IntegrationTests.Fixtures.Builders
+{
+    public static class DreamBuilderExtensions
+    {
+        public static IPostprocessComposer<Dream> DreamBuilder(this Fixture fixture)
+        {
+            return fixture.Build<Dream>()
+                .With(u => u.DreamId, 0)
+                .With(u => u.DreamState, DreamStateEnum.Created)
+                .Without(u => u.DreamCategory)
+                .Without(u => u.DreamCategoryId)
+                .Without(u => u.RequiredSteps)
+                .Without(u => u.Volunteers);
+        }
+
+        public static IPostprocessComposer<Dream> WithNewCategory(this IPostprocessComposer<Dream> postProcessComposer)
+        {
+            return postProcessComposer.WithCategory(new Fixture().DreamCategoryBuilder().Create());
+        }
+
+        public static IPostprocessComposer<Dream> WithCategory(this IPostprocessComposer<Dream> postProcessComposer, DreamCategory category)
+        {
+            return postProcessComposer.With(d => d.DreamCategory, category);
+        }
+    }
+}
