f3bcf9cf148f48feb6cae823f07be366cef4af87
arborQ <arbor@o2.pl>
2020-07-31 18:38:42 +0200

    Feature/34 (#38)
    * default steps
    
    * create category
    
    * add dream unit tests
    
    * add integration tests
    
    * change volumes
    
    * ok
    
    Co-authored-by: Lukasz Wojcik <arbor@10g.pl>
    Co-authored-by: Lukasz Wojcik <lukasz.wojcik@softwareone.com>

---
+++ backend/Alpaki.Database/Migrations/20200731162632_ReseedAdminMigration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Alpaki.Database.Migrations
+{
+    public partial class ReseedAdminMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1L,
+                column: "PasswordHash",
+                value: "AJTp51ZlD0se8DlcjykRO3JHOXwvqConSQdep7P6Xg7TyFF17lu/txQo7Jjn0s+rxQ==");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1L,
+                column: "PasswordHash",
+                value: null);
+        }
+    }
+}
