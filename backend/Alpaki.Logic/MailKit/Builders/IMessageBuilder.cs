ccd47a661410da78e7ab0525f4860c4898f0520e
Grzegorz Gruszczyk <grzesiu15432@gmail.com>
2020-08-16 13:15:11 +0200

    Add service responsible for sending Emails
     * Use MailKit library
     * Use Mailhog for faking smtp server

---
+++ backend/Alpaki.Logic/MailKit/Builders/IMessageBuilder.cs
@@ -0,0 +1,15 @@
+using MimeKit;
+
+namespace Alpaki.Logic.MailKit.Builders
+{
+    public interface IMessageBuilder
+    {
+        IMessageBuilder WithSender(string senderEmail);
+        IMessageBuilder WithReceiver(string receiverEmail);
+        IMessageBuilder WithSubject(string subject);
+        IMessageBuilder WithSubject(string template, params object[] @params);
+        IMessageBuilder WithBody(string body);
+        IMessageBuilder WithBody(string template, params object[] @params);
+        MimeMessage Build();
+    }
+}
