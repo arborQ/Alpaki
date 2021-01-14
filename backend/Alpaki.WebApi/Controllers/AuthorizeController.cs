92a3a00920ba29dbdf91821c0a2720023cb817ef
Lukasz Wojcik <lukasz.wojcik@softwareone.com>
2021-01-14 11:20:32 +0100

    dsa

--- backend/Alpaki.WebApi/Controllers/AuthorizeController.cs
+++ backend/Alpaki.WebApi/Controllers/AuthorizeController.cs
@@ -2,31 +2,12 @@
 using System.Threading.Tasks;
 using Alpaki.CrossCutting.Interfaces;
 using Alpaki.Logic.Handlers.AuthorizeUserPassword;
-using Grpc.Core;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Alpaki.WebApi.Controllers
 {
-    public class AuthorizeService : Authorize.AuthorizeBase
-    {
-        private readonly IMediator _mediator;
-
-        public AuthorizeService(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
-        public override async Task<AuthorizeResponse> ValidateUser(AuthorizeRequest request, ServerCallContext context)
-        {
-            var validateUser = await _mediator.Send(new AuthorizeUserPasswordRequest { Login = request.Login, Password = request.Password });
-
-            return new AuthorizeResponse { Token = validateUser.Token };
-        }
-    }
-
-
     [AllowAnonymous]
     [ApiController]
     [Route("api/[controller]")]
