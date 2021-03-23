eec4191972ecdb327da4ee63b3d38967b5da6712
Lukasz Wojcik <arbor@10g.pl>
2021-03-23 19:20:00 +0100

    port

--- react-frontend/production.server.js
+++ react-frontend/production.server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.port;
 
 app.use(express.static('./'));
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`App listening at https://localhost:${port}`)
 });
