6cb01031636d8431b75b984bad0e081475ef14c8
Lukasz Wojcik <lukasz.wojcik@softwareone.com>
2021-03-25 12:00:34 +0100

    fetch

--- react-frontend/src/Pages/Products/products.list.tsx
+++ react-frontend/src/Pages/Products/products.list.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Card } from 'Components/Card';
 
+const query = {
+    "count":false,
+    "skip":0,
+    "top":50,
+    "searchMode":"any",
+    "queryType":"simple",
+    "facets":["is_available,count:5,sort:count"],
+    "search":"kartka"
+};
+
 export function ProductList() {
+    useEffect(() => {
+        fetch('/api/search?index=party-shop-index', { method: 'POST', body: JSON.stringify(query) })
+    }, []);
     return (
         <div className="w-full flex justify-center">
             <div className="w-full lg:w-3/4 p-4">
